bcacdf5797742b6124dc9215ea8e12b1e9af165e
Cynthia Fortier <cyfor001@edu.cegepgarneau.com>
2023-09-20 15:22:25 -0400

    appreciation models

---
+++ src/lib/models/appreciation.mjs
@@ -0,0 +1,14 @@
+import { Schema, model, ObjectId } from "mongoose";
+import { contraintes } from "../consts.mjs";
+
+
+
+export const Appreciation = new Schema({
+	note: {
+		type: Number,
+		min: 1,
+		max: 5,
+	}
+});
+
+export default Appreciation;
