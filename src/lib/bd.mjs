5bf0b2b2795a0a6d516a23ac74cc8461ecedf096
Pascal <93993227+vermarine@users.noreply.github.com>
2023-09-22 11:08:39 -0400

    bd: simplifier la gestion des collections

--- src/lib/bd.mjs
+++ src/lib/bd.mjs
@@ -1,7 +1,10 @@
 // Importation du paquet NPM pour MongoDB:
-import mongoose from "mongoose";
-import ModeleSession from "./models/session.mjs";
-import ModeleUsager from "./models/usager.mjs";
+import mongoose, { Model, model } from "mongoose";
+
+// Importation des schémas:
+import SchemaSession from "./models/session.mjs";
+import SchemaUsager from "./models/usager.mjs";
+import SchemaAppreciation from "./models/appreciation.mjs";
 
 // Importation des interfaces:
 /** @typedef {import("./interfaces.mjs").IDateEvenement} IDateEvenement */
@@ -21,88 +24,28 @@ if (urlConnexion == null || urlConnexion.length < 1 || nomBase == null || nomBas
 	throw new Error("Veuillez configurer un fichier .env contenant une clé MONGO_URL.");
 }
 
+// Connexion à mongo avec la connexion Mongoose par défaut:
+await mongoose.connect(urlConnexion);
+await mongoose.connection.useDb(nomBase);
+
+// Création des modèles à partir des schémas:
+const Session = model("Session", SchemaSession);
+const Usager = model("Usager", SchemaSession);
+const Appreciation = model("Appreciation", SchemaAppreciation);
+
 
 /**
  * Classe de communication avec la base MongoDB
  */
 export default class Bd {
-	/***************************************************************************
-	 ************************* Propriétés statiques ****************************
-	 **************************************************************************/
-
-	/**
-	 * Référence de connexion à MongoDB
-	 * @type {mongoose.Connection}
-	 */
-	static #client = null;
-
-	/**
-	 * Référence aux collections de la base de données.
-	 * @type {Map<String,mongoose.Model>}
-	 */
-	static #collections = new Map();
-
-
-
-	/***************************************************************************
-	 *************************** Méthodes privées ******************************
-	 **************************************************************************/
-
-	/**
-	 * Établit une connexion avec la base de données si ce n'est pas déjà fait.
-	 */
-	static async #etablirConnexionAvecBase() {
-		// Si déjà connecté, nul besoin de reconnecter:
-		if (Bd.#client !== null) {
-			return;
-		}
-
-		// Connexion au serveur MongoDB:
-		Bd.#client = await mongoose.createConnection(urlConnexion);
-
-		// Connexion à la base de données:
-		Bd.#client.useDb(nomBase);
-	}
-
-	/**
-	 * Initialise les modèles et collections de la base MongoDB
-	 */
-	static async #initialiserModeles() {
-		await this.#etablirConnexionAvecBase();
-
-		Bd.#collections
-			.set("sessions", Bd.#client.model("Session", ModeleSession))
-			.set("usagers", Bd.#client.model("Usager", ModeleUsager));
-	}
-
-	/**
-	 * Obtient une référence à la collection donnée en paramètre.
-	 * @param {String} nom nom de la collection
-	 * @returns {Promise<mongoose.Model>} Une référence à la collection MongoDB
-	 */
-	static async #obtenirCollection(nom) {
-		if (Bd.#collections.size < 1) {
-			await Bd.#initialiserModeles();
-		}
-
-		return Bd.#collections.get(nom);
-	}
-
-
-
-	/***************************************************************************
-	 ************************** Méthodes publiques *****************************
-	 **************************************************************************/
-
 	/**
 	 * Obtient une session ayant la clé donnée.
 	 *
 	 * @param {String} cleSession clé publique de la session
-	 * @returns {typeof ModeleSession}
+	 * @returns {typeof SchemaSession}
 	 */
 	static async obtenirSession(cleSession) {
-		const sessions = await Bd.#obtenirCollection("sessions");
-		return await sessions.findOne({ cle: cleSession, dateExpiration: { $gt: new Date() } });
+		return await Session.findOne({ cle: cleSession, dateExpiration: { $gt: new Date() } });
 	}
 
 	/**
@@ -111,9 +54,8 @@ export default class Bd {
 	 * @param {String} idUsager identifiant unique de l'usager
 	 */
 	static async supprimerSession(idUsager) {
-		const sessions = await Bd.#obtenirCollection("sessions");
 		const usager = new ObjectId(idUsager);
-		await sessions.deleteOne({ usager: usager });
+		await Session.deleteOne({ usager: usager });
 	}
 
 	/**
@@ -122,13 +64,12 @@ export default class Bd {
 	 * @param {String} cleSession clé publique de la session
 	 * @param {String} idUsager identifiant unique de l'usager
 	 * @param {Date} expiration date d'expiration de la session
-	 * @returns {typeof ModeleSession}
+	 * @returns {typeof SchemaSession}
 	 */
 	static async nouvSession(cleSession, idUsager, expiration) {
-		const sessions = await Bd.#obtenirCollection("sessions");
 		await Bd.supprimerSession(idUsager);
 		const usager = new ObjectId(idUsager);
-		const session = new sessions({
+		const session = new Session({
 			cle: cleSession,
 			usager: usager,
 			dateExpiration: expiration
@@ -136,16 +77,5 @@ export default class Bd {
 		await session.save();
 
 		return session;
-	}
-
-	/**
-	 * Obtient tous les champs d'un document de la collection `danses`
-	 *
-	 * @param {String} permalien Le permalien de la danse
-	 * @returns Les détails de la danse
-	 */
-	static async obtenirDetailsDanse(permalien) {
-		const danses = await Bd.#obtenirCollection("danses");
-		return await danses.findOne({ permalien: permalien });
 	}
 }
