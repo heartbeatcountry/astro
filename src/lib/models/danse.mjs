f63eedf4e35d1346b5b182725ad7ebaf73ed1bc4
Cynthia Fortier <cyfor001@edu.cegepgarneau.com>
2023-09-23 23:44:06 -0400

    Ajout Model danse et cours, début affichage danses sur l'accueil

---
+++ src/lib/models/danse.mjs
@@ -0,0 +1,77 @@
+import { Schema, model, ObjectId } from "mongoose";
+import { contraintes } from "../consts.mjs";
+
+
+export const Danse = new Schema({
+
+	titre: {
+		type: String,
+		trim: true,
+		required: true,
+		minLength: contraintes.danse.titre.longueurMin,
+		maxLength: contraintes.danse.titre.longueurMax,
+	},
+	choregraphe: {
+		type: String,
+		trim: true,
+		required: "Le prénom et nom du chorégraphe est requis.  Sinon indiquer inconnu.",
+		minLength: contraintes.danse.choregraphe.longueurMin,
+		maxLength: contraintes.danse.choregraphe.longueurMax,
+	},
+	musique: {
+		type: String,
+		trim: true,
+		required: true,
+		minLength: contraintes.danse.musique.longueurMin,
+		maxLength: contraintes.danse.musique.longueurMax,
+	},
+	niveau:{
+		type:String,
+		enum: ['Initiation', 'Débutant','Novice','Intermédiaire','Avancé'],
+		required: true,
+	},
+	detailsTag: {
+		type: String,
+		trim: true,
+		minLength: contraintes.danse.detailsTag.longueurMin,
+		maxLength: contraintes.danse.detailsTag.longueurMax,
+	},
+	lienFeuille: {
+		type: String,
+		trim: true,
+	},
+	lienVideoDanse: {
+		type: String,
+		trim: true,
+	},
+	lienVideoMusique: {
+		type: String,
+		trim: true,
+	},
+	lienVideoAcademie: {
+		type: String,
+		trim: true,
+	},
+	nbComptes: {
+		type: Number,
+		min: 8,
+		max: 255,
+	},
+	nbMurs: {
+		type: Number,
+		min: 0,
+		max: 4,
+	},
+	estCoupDeCoeur: {
+		type: Boolean,
+		default: false,
+	},
+	cours:{
+		type: [ObjectId],
+		ref: 'Cours',
+		required: true,
+		unique: true,
+	}
+});
+
+export default Danse;
