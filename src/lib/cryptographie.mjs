463e1eec9f74e6494a032715f7d65be84eb10a95
Pascal Breton <93993227+vermarine@users.noreply.github.com>
2023-09-25 11:26:10 -0400

    ajouter des fonctions pour hacher et valider les mdps

--- src/lib/cryptographie.mjs
+++ src/lib/cryptographie.mjs
@@ -1,6 +1,9 @@
 // importation du paquet NodeJS pour la cryptographie:
 import { randomBytes } from "node:crypto";
 
+// importation du paquet NPM pour argon2-rs:
+import { hash, verify } from "@node-rs/argon2";
+
 /**
  * Génère une chaîne aléatoire selon une entropie donnée.
  *
@@ -9,3 +12,33 @@ import { randomBytes } from "node:crypto";
  */
 export const chaineAleatoire = (octets = 64) =>
 	randomBytes(octets).toString("base64url");
+
+/**
+ * Options à utiliser pour le chiffrement argon2
+ *
+ * @type {import("@node-rs/argon2").Options}
+ */
+const optionsArgon2 = {
+	timeCost: 6,
+	outputLen: 64,
+};
+
+/**
+ * Permet de hacher un mot de passe en clair avec Argon2id
+ *
+ * @param {String} mdp mot de passe en clair à hacher
+ * @returns {Promise<String>} le mot de passe haché
+ */
+export const hacherMdp = async mdp =>
+	await hash(mdp, optionsArgon2);
+
+/**
+ * Permet de valider si un mot de passe en clair donné est identique à un
+ * mot de passe précédemment haché avec hacherMdp
+ *
+ * @param {String} mdp mot de passe en clair à valider
+ * @param {String} mdpConnu mot de passe haché à partir duquel on valide
+ * @returns {Promise<Boolean>} vrai si le mot de passe est identique
+ */
+export const validerMdp = async (mdp, mdpConnu) =>
+	await verify(mdpConnu, mdp, optionsArgon2);
