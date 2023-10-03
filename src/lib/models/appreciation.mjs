0afa38a1c7e664e7578b1d02ea9ed50d2a097c1a
Cynthia Fortier <cyfor001@edu.cegepgarneau.com>
2023-10-03 10:02:59 -0400

    Ajout danse dans modèle appréciation

--- src/lib/models/appreciation.mjs
+++ src/lib/models/appreciation.mjs
@@ -1,14 +1,17 @@
 import { Schema, model, ObjectId } from "mongoose";
 import { contraintes } from "../consts.mjs";
 
-
-
 export const Appreciation = new Schema({
 	note: {
 		type: Number,
 		min: 1,
 		max: 5,
-	}
+	},
+
+	danse: {
+		type: ObjectId,
+		ref: "Danse",
+	},
 });
 
 export default Appreciation;
