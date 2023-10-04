99b13e31c166f8c8de68ba730a4d98e10fad633a
Cynthia Fortier <cyfor001@edu.cegepgarneau.com>
2023-10-04 08:44:28 -0400

    Modification des constantes dans le modèle danse

--- src/lib/consts.mjs
+++ src/lib/consts.mjs
@@ -2,7 +2,9 @@
 /**
  * Expression rationnelle pour valider les adresses courriel (RFC 5322)
  */
-const regexCourriel = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i);
+const regexCourriel = new RegExp(
+	/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i
+);
 
 /**
  * Contraintes de validation.
@@ -37,7 +39,7 @@ export const contraintes = {
 
 		telephone: {
 			regex: /^\(?([0-9]{3})\)?[- ]?([0-9]{3})[-]?([0-9]{4})$/,
-		}
+		},
 	},
 
 	/**
@@ -55,30 +57,37 @@ export const contraintes = {
 		/**
 		 * Prénom et nom du ou des chorégraphes
 		 */
-		choregraphe:{
+		choregraphe: {
 			longueurMin: 8,
 			longueurMax: 255,
 		},
 		/**
 		 * Musique
 		 */
-			musique: {
-				longueurMin: 1,
-				longueurMax: 255,
-			},
+		musique: {
+			longueurMin: 1,
+			longueurMax: 255,
+		},
 		/**
 		 * Détails des "Tags" et "Restarts"
 		 */
-		detailsTag:{
+		detailsTag: {
 			longueurMin: 8,
 			longueurMax: 555,
-		}
-
+		},
+		/**
+		 * Détails du nombre de comptes
+		 */
+		nbComptes: {
+			min: 8,
+			max: 255,
+		},
+		/**
+		 * Détails du nombre de murs
+		 */
+		nbMurs: {
+			min: 1,
+			max: 4,
+		},
 	},
-
-
-
-
-
-
 };
